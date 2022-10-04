230344f0cd98c0f198b14f49e86af61b9b9360dd
Anurag Mathur <anurag.mathur888@gmail.com>
2022-10-05 00:01:37 +0200

    Update and rename DefiningInteger.py to VariableDeclaration.py
    Python is entirely object-oriented, and not "statically typed". You do not need to declare variables or their type before using them. Every variable in Python is an object.
    added more Variable declaration

--- DefiningInteger.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct  4 23:52:11 2022
-
-@author: Anurag
-"""
-
-myint = 7
-print(myint)
---
+++ VariableDeclaration.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  4 23:52:11 2022
+
+@author: Anurag
+"""
+
+myInt = 7
+print(myInt)
+
+myFloat = 7.0
+print(myFloat)
+myFloat = float(7)
+print(myFloat)
+
+myString = 'hello'
+print(myString)
+myString = "hello"
+print(myString)
+
+myString = "This also works"
+print(myString)
+
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
